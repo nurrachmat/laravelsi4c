2bc092befdeb115662369b6f6595fe784db46826
nurrachmat-nr <rachmat.nur@gmail.com>
2026-05-11 13:11:16 +0700

    Initial commit

---
+++ resources/views/fakultas/index.blade.php
@@ -0,0 +1,22 @@
+@extends('main')
+
+@section('title', 'Fakultas')
+
+@section('content')
+<table class="table table-bordered table-hover">
+    <tr>
+        <th>Nama Fakultas</th>
+        <th>Singkatan</th>
+        <th>Dekan</th>
+    </tr>
+    
+    @foreach($result as $item)
+    <tr>
+        <td>{{ $item->nama_fakultas }}</td>
+        <td>{{ $item->singkatan }}</td>
+        <td>{{ $item->dekan }}</td>
+    </tr>
+    @endforeach
+    
+</table>
+@endsection
